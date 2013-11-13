5e8b58ac346d19c236ac51501991e7f5c7b7d99f
Marin <mgs@jpisoftware.net>
2013-11-13 10:22:33 -0800

    auth function and dictionary select

--- main.groovy
+++ main.groovy
@@ -1,91 +1,159 @@
-
 package grr
 
 @Grab(group='org.tinyradius', module='tinyradius', version='0.9.9')
 @Grab(group='commons-cli', module='commons-cli', version='1.2')
 
+
 import org.tinyradius.packet.*
 import org.tinyradius.*
+import org.tinyradius.dictionary.*
 import org.tinyradius.util.RadiusClient
 import org.apache.commons.cli.Options
-import org.apache.commons.cli.PosixParser
+import org.apache.commons.cli.GnuParser
 import org.apache.commons.cli.HelpFormatter
+
+
+def options = new Options()
+options.addOption("c", "config", true, "config file. [./config]")
+options.addOption("h", "host", true, "radius server")
+options.addOption("s", "secret", true, "radius secret")
+options.addOption("d", "dictionary", true, "dictionary file. [./dictionary]")
+options.addOption("help", false, "print usage")
+
+def parser = new GnuParser()
+args = parser.parse(options, args)
+
+// missing args, print help and quit
+if (args.getArgList().size() == 0 || args.hasOption("help")) {
+	help(options)
+	return
+}
+
+// TODO can make this less clear by combining cfgParsed() and dictionary()?
+
+// use default config or passed -c arg
+def cfgParsed =
+{
+	File cfgFile
+	if (args.hasOption("config"))
+		cfgFile = new File(args.getOptionValue("config"))
+	else 
+		cfgFile = new File("config")
+
+	// slurp config file or cfgParsed is false
+	cfgParsed = cfgFile.exists()
+	if (cfgParsed)
+		cfgParsed = new ConfigSlurper().parse(cfgFile.toURL())
+
+	return cfgParsed
+}()
+
+// read default dictionary or passed -d arg
+def dictionary =
+{
+	File dictFile
+	if (args.hasOption("dictionary"))
+		dictFile = new File(args.getOptionValue("dictionary"))
+	else
+		dictFile = new File("dictionary")
+
+	dictionary = dictFile.exists()
+	if (dictionary)
+		dictionary = new FileInputStream(dictFile)
+
+	return dictionary
+}()
+
+Map cfg = [:]  // our final config - will be passed to Client
+
+// add attributes and dictionary into our into o
+if (cfgParsed && cfgParsed.attributes)
+	cfg.attributes = cfgParsed.attributes
+
+if (dictionary)
+	cfg.dictionary = dictionary
+else {
+	println "missing dictionary file. exiting."
+	return
+}
+
+def populateConfig = 
+{
+	def addRequiredCfg = 
+	{ opt, val = null ->
+		if (val != null) 						   // if passed val isnt nul
+			cfg[opt] = val						   // set passed option
+		else if(args.hasOption(opt))
+			cfg[opt] = args.getOptionValue(opt)	   // set cli option passed
+		else if (cfgParsed == false|| !cfgParsed[opt])	
+			println "missing reqired option: $opt" // required option not present anywhere
+		else
+			cfg[opt] = cfgParsed[opt]			   // set from config file
+	}
+
+	addRequiredCfg("host")
+	addRequiredCfg("secret")
+	addRequiredCfg("action", args.getArgList()[0])
+	addRequiredCfg("username", args.getArgList()[1])
+	addRequiredCfg("password", args.getArgList()[2])
+}()
+
+
+new Client(cfg)
 
 class Client
 {
 	Map cfg
+	Dictionary dictionary
+	RadiusClient client
 
 	Client(Map cfg)
 	{
 		client = new RadiusClient(cfg.host, cfg.secret)
 		this.cfg = cfg
+		dictionary = DictionaryParser.parseDictionary(cfg.dictionary)
+
+		try {
+			this[cfg.action]()
+		}catch (ExceptionInInitializerError exp) {
+			println("error staring tinyradius - make sure your jar has default dictionary in it: see http://github.com/hoffoo/grr-radius/issues/1")
+		}
 	}
 	
-	// regular authenticate, returns a response packet
-	RadiusPacket auth(username, password)
+	def auth =
 	{
-		def request = authReq(username, password)
+		def request = new AccessRequest(cfg.username, cfg.password)
+		request.setDictionary(dictionary)
+		request.setAuthProtocol(AccessRequest.AUTH_CHAP)
+
+		cfg.attributes.each
+		{ k, v -> 
+			request.addAttribute(k, v)
+		}
+
 		def result = client.authenticate(request)
 		client.close()
 
 		return result
 	}
-
-	AccessRequest authReq(username, password)
-	{
-		def request = new AccessRequest(username, password)
-		request.setAuthProtocol(AUTH_TYPE)
-		request.addAttribute("NAS-Identifier", cfg.nasIdentifier)
-		request.addAttribute("NAS-IP-Address", cfg.nasIp)
-		request.addAttribute("Service-Type", cfg.serviceType)
-
-		return request
-	}
 }
 
-
-def options = new Options()
-options.addOption("c", "config", true, "config file")
-options.addOption("h", "host", true, "radius server")
-options.addOption("s", "secret", true, "radius secret")
-
-def parser = new PosixParser()
-args = parser.parse(options, args)
-if (args.getArgList().size() != 2) {
-	help(options)
-	return
-}
-
-File cfgFile
-
-if (!args.hasOption("config"))
-	cfgFile = new File("config")
-else 
-	cfgFile = new File(args.getOptionValue('config'))
-
-def cfgParsed = cfgFile.exists()
-if (cfgParsed)
-	cfgParsed = new ConfigSlurper().parse(cfgFile.toURL())
-
-Map cfg = [:]
-def addRequiredCfg = 
-{ opt ->
-	if (args.hasOption(opt))
-		cfg[opt] = args.getOptionValue(opt)
-	else if (!cfgParsed || !cfgParsed[opt])
-		println "missing reqired option: $opt"
-	else
-		cfg[opt] = cfgParsed[opt]
-}
-
-addRequiredCfg("host")
-addRequiredCfg("secret")
-
-
-
-void help(options)
+static void help(options)
 {
+	// sort long options at the bottom
+	def comparator = [compare:
+	{ a, b ->
+		if (a.opt.size() + b.opt.size() == 2) {
+			a.opt < b.opt ? -1 : 1
+		} else {
+			a.opt.size() > b.opt.size() ? 1 : -1
+		}
+
+	}] as Comparator
+
 	def formatter = new HelpFormatter()
+	formatter.setOptionComparator(comparator)
+
 	formatter.printHelp(
 		80,
 		"grr [options] action username password",
